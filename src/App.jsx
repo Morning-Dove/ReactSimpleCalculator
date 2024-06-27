89a1186c4519007f3f48c108c6f054ac6c8b6182
Morning Dove Nelson <morningdhansen@gmail.com>
2024-06-27 11:07:22 -0600

    working calculator

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
 import './App.css'
+import Calculator from "./Calculator";
 
 function App() {
 
   return (
     <>
+    <header>
+      <h1>Simple Calculator</h1>
+    </header>
+    <Calculator />
+    <footer>
+      <p>Created By: Morning Dove Nelson 2024</p>
+    </footer>
     </>
   );
 }
